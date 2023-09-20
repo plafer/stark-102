6d9ffac85c445e89022f3dbc25eae8cbd42ef62e
Philippe Laferriere <plafer@protonmail.com>
2023-09-20 08:36:01 -0400

    add `random_integer()` to channel

--- src/channel.rs
+++ src/channel.rs
@@ -28,23 +28,36 @@ impl Channel {
         self.current_hash = hasher.finalize();
     }
 
+    /// Draws a random element from `BaseField` (i.e. a number between 0 and 16).
     pub fn random_element(&mut self) -> BaseField {
         let hash_first_4_bytes: [u8; 4] = self.current_hash.as_bytes()[0..4].try_into().unwrap();
         let ret_element: BaseField = i32::from_le_bytes(hash_first_4_bytes).into();
 
-        // this is an arbitrary way to change the current hash, so that we can
-        // call `random_element()` multiple times and always get a different one
-        {
-            let mut hasher = Hasher::new();
-            hasher.update(self.current_hash.as_bytes());
-            hasher.update(&self.count.to_le_bytes());
-
-            self.count += 1;
-
-            self.current_hash = hasher.finalize();
-        }
+        self.rehash_after_draw();
 
         ret_element
+    }
+
+    /// Draws a random integer (uniformly distributed) in the range [0, upper_bound-1].
+    pub fn random_integer(&mut self, upper_bound: u8) -> u8 {
+        let hash_first_byte: [u8; 1] = self.current_hash.as_bytes()[0..1].try_into().unwrap();
+        let ret_element = u8::from_le_bytes(hash_first_byte) % upper_bound;
+
+        self.rehash_after_draw();
+
+        ret_element
+    }
+
+    /// this is an arbitrary way to change the current hash, so that we can call
+    /// `random_element()` multiple times and always get a different one
+    fn rehash_after_draw(&mut self) {
+        let mut hasher = Hasher::new();
+        hasher.update(self.current_hash.as_bytes());
+        hasher.update(&self.count.to_le_bytes());
+
+        self.count += 1;
+
+        self.current_hash = hasher.finalize();
     }
 
     // Closes the channel, returning the commitments to be used in the final StarkProof
