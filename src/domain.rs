69da3c2fd465892a0c40f649a5274093df381c1d
Philippe Laferriere <plafer@protonmail.com>
2023-09-25 09:45:50 -0400

    Refactor Domain struct

--- src/domain.rs
+++ src/domain.rs
@@ -2,7 +2,8 @@ use std::ops::{Deref, Index};
 
 use crate::field::BaseField;
 
-pub static DOMAIN_TRACE: DomainTrace = DomainTrace {
+/// TODO: Represents domain trace, and what that is
+pub static DOMAIN_TRACE: Domain<4, 13> = Domain {
     elements: [
         BaseField::new(1),
         BaseField::new(13),
@@ -11,17 +12,32 @@ pub static DOMAIN_TRACE: DomainTrace = DomainTrace {
     ],
 };
 
-pub struct DomainTrace {
-    elements: [BaseField; 4],
+/// TODO: How we get that domain (coset)
+pub static DOMAIN_LDE: Domain<8, 9> = Domain {
+    elements: [
+        BaseField::new(3),
+        BaseField::new(10),
+        BaseField::new(5),
+        BaseField::new(11),
+        BaseField::new(14),
+        BaseField::new(7),
+        BaseField::new(12),
+        BaseField::new(6),
+    ],
+};
+
+/// TODO: Document generic params
+pub struct Domain<const N: usize, const GENERATOR: u8> {
+    elements: [BaseField; N],
 }
 
-impl DomainTrace {
+impl<const N: usize, const GENERATOR: u8> Domain<N, GENERATOR> {
     pub const fn generator() -> BaseField {
-        BaseField::new(13)
+        BaseField::new(GENERATOR)
     }
 }
 
-impl Index<usize> for DomainTrace {
+impl<const N: usize, const GENERATOR: u8> Index<usize> for Domain<N, GENERATOR> {
     type Output = BaseField;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -29,7 +45,7 @@ impl Index<usize> for DomainTrace {
     }
 }
 
-impl Deref for DomainTrace {
+impl<const N: usize, const GENERATOR: u8> Deref for Domain<N, GENERATOR> {
     type Target = [BaseField];
 
     fn deref(&self) -> &Self::Target {
